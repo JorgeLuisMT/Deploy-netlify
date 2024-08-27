90f63648d29bda2c0200ac1397a6436c628ecf38
JorgeLuisMT <jorgeluismercado003@gmail.com>
2024-08-26 17:46:28 -0700

    jjj

--- router/movies.js
+++ router/movies.js
@@ -5,7 +5,7 @@ import { MovieController } from "../controllers/movies";
 export const moviesRouter = Router();
 
 moviesRouter.get("/", MovieController.getAll);
-moviesRouter.post("/", MovieController.getAll);
+moviesRouter.post("/", MovieController.create);
 
 moviesRouter.get("/:id", MovieController.getById);
 moviesRouter.delete("/:id", MovieController.delete);
