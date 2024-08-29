7699cd29eb63f52db6ae97862104c50ac9b7155a
JorgeLuisMT <jorgeluismercado003@gmail.com>
2024-08-29 14:17:18 -0700

    jjj

--- server-sql.js
+++ server-sql.js
@@ -1,5 +1,5 @@
 import { createApp } from "./functions/app.js";
-import ServerlessHttp from "serverless-http";
+
 import { MovieModel } from "./models/serverSQL/movie.js";
 
-export const handler = ServerlessHttp(createApp({ movieModel: MovieModel }));
+createApp({ movieModel: MovieModel });
