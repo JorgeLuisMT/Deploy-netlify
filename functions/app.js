87d8f8fc0a00d80761de67b741d2cc9f55501b6c
JorgeLuisMT <jorgeluismercado003@gmail.com>
2024-08-29 15:13:38 -0700

    kjk

--- functions/app.js
+++ functions/app.js
@@ -22,14 +22,9 @@ export const createApp = ({ movieModel }) => {
     res.send(200);
   }); */
 
-  api.use(
-    "/.netlify/functions/server-sql/movies",
-    createMovieRouter({ movieModel })
-  );
+  api.use("/.netlify/functions/app/movies", createMovieRouter({ movieModel }));
 
   const PORT = process.env.PORT ?? 3001;
 
-  //api.listen(PORT, () => console.log(`http://localhost:${PORT}`));
-
-  return api;
+  api.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 };
--- functions/server-sql.js
+++ functions/server-sql.js
@@ -2,6 +2,4 @@ import { createApp } from "./app.js";
 
 import { MovieModel } from "../models/serverSQL/movie.js";
 
-import ServerlessHttp from "serverless-http";
-
 createApp({ movieModel: MovieModel });
