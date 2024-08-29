a3c168f8bf8b8a3bdd325af33cc312047e0e93e9
JorgeLuisMT <jorgeluismercado003@gmail.com>
2024-08-29 14:37:30 -0700

    aaaa

--- functions/server-sql.js
+++ functions/server-sql.js
@@ -4,4 +4,6 @@ import { MovieModel } from "../models/serverSQL/movie.js";
 
 import ServerlessHttp from "serverless-http";
 
-export const handler = ServerlessHttp(createApp({ movieModel: MovieModel }));
+const api = createApp({ movieModel: MovieModel });
+
+export const handler = ServerlessHttp(api);
